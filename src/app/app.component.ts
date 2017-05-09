14e33e1b622269605e189ea2b27a51acff7848f0
gokemon <michael.of.gokey@gmail.com>
2017-05-09 13:24:59 -0400

    Display a list of staff, and allow users to select a person and display the person's details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,23 @@ export class Hero {
   name: string;
 }/* I would think this goes somewhere else */
 
+/* I guess this constructor also goes somewhere else later
+as it is not part of the app.component */
+const HEROES: Hero[] = [
+  { id: 11, name: 'George Marshall' },
+  { id: 12, name: 'Dwight D. Eisenhower' },
+  { id: 13, name: 'Douglas MacArthur' },
+  { id: 14, name: 'Omar Bradley' },
+  { id: 15, name: 'Mark W. Clark' },
+  { id: 16, name: 'Harold Alexander' },
+  { id: 17, name: 'Courtney Hodges' },
+  { id: 27, name: 'Matthew B. Ridgeway' },
+  { id: 82, name: 'James M. Gavin' },
+  { id: 20, name: 'William Orlando Darby' },
+  { id: 21, name: 'Robert T. Frederick' },
+  { id: 101, name: 'Anthony McAuliffe' }
+];
+
 
 // TypeDecorator
 @Component({
@@ -17,10 +34,16 @@ export class Hero {
 // exportable class
 export class AppComponent {
   title = 'Hero Staffing';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // }; // moved out
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }// AppComponent class
 
 
